b3e94b1db79434c95ce06a721463d1815d25d90b
Vikram Reddy <vikram.s.reddy@oracle.com>
2023-07-29 12:50:30 +0530

    first commit

---
+++ spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.javatechie.ps.api.controller;
+
+import com.javatechie.ps.api.entity.Payment;
+import com.javatechie.ps.api.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @PostMapping("/doPayment")
+    public Payment doPayment(@RequestBody Payment payment) {
+        return service.doPayment(payment);
+    }
+
+    @GetMapping("/{orderId}")
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId){
+              return service.findPaymentHistoryByOrderId(orderId);
+    }
+
+
+}
